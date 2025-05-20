afc63091f5f6a265942e9c532bafe47737f9dca4
Muhamad Novreysa <43816986+MuhamadNovreysa@users.noreply.github.com>
2025-05-20 14:34:17 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,26 @@
+    </div>
+    <footer class="bg-dark text-white mt-5 py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h5>KONTRAKAN DIGITAL</h5>
+                    <p>Solusi digital untuk pencarian dan pengelolaan kontrakan.</p>
+                </div>
+                <div class="col-md-4">
+                    <h5>Kontak Kami</h5>
+                    <p><i class="fas fa-envelope"></i> info@kontrakandigital.com</p>
+                    <p><i class="fas fa-phone"></i> +62 123 4567 890</p>
+                </div>
+                <div class="col-md-4">
+                    <h5>Follow Kami</h5>
+                    <a href="#" class="text-white me-2"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="text-white me-2"><i class="fab fa-twitter"></i></a>
+                    <a href="#" class="text-white me-2"><i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
